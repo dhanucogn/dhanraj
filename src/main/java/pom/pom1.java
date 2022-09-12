6d401ce7241e202348c41121b281e8882b84a0ad
dhanucogn <113376950+dhanucogn@users.noreply.github.com>
2022-09-12 10:17:59 -0700

    Update pom1.java

--- src/main/java/pom/pom1.java
+++ src/main/java/pom/pom1.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+// ppooomm
 public class pom1 {
 	//declaration
 	@FindBy(xpath ="//input[@id='userid']")private WebElement user;
